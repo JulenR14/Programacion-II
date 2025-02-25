7df820a95cb0cc058a948f7345838029031dc09a
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2025-02-25 18:16:07 +0100

    Terminando el ejercicio 13

--- EIMT1008/src/Practica1/Ejercicio13.java
+++ EIMT1008/src/Practica1/Ejercicio13.java
@@ -2,7 +2,14 @@ package Practica1;
 
 public class Ejercicio13 {
     public static void main(String[] args){
-
+        prueba("hola como estas", 1, "hola");
+        prueba("hola como estas", 2, "como");
+        prueba("hola como estas", 3, "estas");
+        prueba("hola como estas", 5, null);
+        prueba("  hola como estas", 1, "hola");
+        prueba("  hola como estas", 2, "como");
+        //System.out.println("Longitud : " + "  hola  como  estas ".length());
+        //prueba("  hola  como  estas ", 3, "estas");
     }
 
     public static String obtenerPalabra(String cadena, int numero){
@@ -11,9 +18,34 @@ public class Ejercicio13 {
         String palabra = null;
         if (numero <= numeroPalabras) {
             int contPalabras = 0;
-            
+            int recorrerCadena = 0;
+            String crearPalabra = "";
+            while ((recorrerCadena <= cadena.length())) {
+                if (recorrerCadena != 0) {
+                    if (cadena.charAt(recorrerCadena - 1) == ' ' && cadena.charAt(recorrerCadena) != ' ') {
+                        contPalabras++;
+                        if (contPalabras == numero) {
+                            int cCaracter = 0;
+                            while(((recorrerCadena+cCaracter) < cadena.length()) && (cadena.charAt(recorrerCadena+cCaracter) != ' ')){
+                                crearPalabra += cadena.charAt(recorrerCadena+cCaracter);
+                                cCaracter++;
+                            }
+                        }
+                    }
+                } else if (cadena.charAt(recorrerCadena) != ' ') {
+                    contPalabras++;
+                    if (contPalabras == numero) {
+                        int cCaracter = 0;
+                        while(cadena.charAt(recorrerCadena+cCaracter) != ' '){
+                            crearPalabra += cadena.charAt(recorrerCadena+cCaracter);
+                            cCaracter++;
+                        }
+                    }
+                }
+                recorrerCadena++;
+            }
+            palabra = crearPalabra;
         }
-
         return palabra;
     }
 
@@ -37,9 +69,9 @@ public class Ejercicio13 {
         String resultado = obtenerPalabra(cadena, numeroEntero);
 
         System.out.format("Prueba con %s y %s --> %s: ", cadena, numeroEntero ,resultado);
-        if (resultado == esperado)
+        if ((esperado==null && resultado==null) || resultado.equals(esperado))
             System.out.println("ok");
         else
-            System.out.format("error (se esperaba%s)%n", esperado);
+            System.out.format("error (se esperaba %s)%n", esperado);
     }
 }
