9206bf897dec3a5117b65e4f365f3bfd551dea15
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2025-03-02 19:35:57 +0100

    Realizado el ejercicio 15

--- EIMT1008/src/Practica1/Ejercicio15.java
+++ EIMT1008/src/Practica1/Ejercicio15.java
@@ -4,12 +4,21 @@ import java.util.Arrays;
 
 public class Ejercicio15 {
     public static void main(String[] args){
-
+        prueba(new String[]{"aa", "bb", "cc", "dd"}, true);
+        prueba(new String[]{"aa", "bb", "cc", "dd"}, true);
+        prueba(new String[]{"dfa", "a", "d", "a"}, false);
+        prueba(new String[]{"bb", "bb", "cc", "dd"}, true);
     }
 
     public static boolean estaOrdenadoMenorMayor(String[] vector){
         boolean ordenado = true;
-
+        String valorAcomparar = vector[0];
+        for (int i=1; i<vector.length;i++){
+            if(valorAcomparar.compareTo(vector[i]) > 0){
+                ordenado = false;
+            }
+            valorAcomparar = vector[i];
+        }
         return ordenado;
     }
 
