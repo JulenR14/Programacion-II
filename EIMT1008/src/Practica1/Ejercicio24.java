d8eebba12f2171b8a5fdb9852e6ed4a83a345f6b
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2025-03-04 13:58:38 +0100

    Creado la clase para el 24

---
+++ EIMT1008/src/Practica1/Ejercicio24.java
@@ -0,0 +1,11 @@
+package Practica1;
+
+public class Ejercicio24 {
+    public static void main(String[] args){
+
+    }
+
+    public static int[][] crearMatrizCombinaciones(){
+        return new int[][]{};
+    }
+}
