e2f74ef2cbbb9a9ecd1639dc46dc6211ce98f3c9
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2025-03-09 09:52:35 +0100

    He creado la matriz, pero aun no devuelve lo esperado

--- EIMT1008/src/Practica1/Ejercicio_24/Ejercicio24.java
+++ EIMT1008/src/Practica1/Ejercicio_24/Ejercicio24.java
@@ -2,11 +2,15 @@ package Practica1.Ejercicio_24;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio24 {
     public static void main(String[] args) throws FileNotFoundException {
-        crearMatrizCombinaciones(new String[]{}, "./src/Practica1/Ejercicio_24/alquimia10.txt");
+        int[][] matriz = crearMatrizCombinaciones(new String[]{"tierra", "fuego" ,"lava" ,"aire" ,"piedra" ,"agua", "arena" ,"cristal" ,"reloj_de_arena"}, "./src/Practica1/Ejercicio_24/alquimia10.txt");
+        for (int i = 0; i < matriz.length; i++){
+            System.out.println(Arrays.toString(matriz[i]));
+        }
     }
 
     public static int[][] crearMatrizCombinaciones(String[] vector, String nombreFichero) throws FileNotFoundException {
@@ -17,18 +21,22 @@ public class Ejercicio24 {
                 Scanner sc = new Scanner(new File(nombreFichero));
                 String elemento1 = vector[i];
                 String elemento2 = vector[j];
-                while (sc.hasNext()){
-                    String[] lineaElementos = sc.nextLine().split(" ");
+                if (sc.hasNext()){
+                    //String[] lineaElementos = sc.nextLine().split(" ");
+                   String[] lineaElementos = new String[]{sc.next(), sc.next(), sc.next()};
                     if (elemento1.equals(lineaElementos[0]) && elemento2.equals(lineaElementos[1]) ||
                         elemento2.equals(lineaElementos[0]) && elemento1.equals(lineaElementos[1]) ||
                         elemento1.equals(lineaElementos[0]) && elemento1.equals(lineaElementos[1]) ||
                         elemento2.equals(lineaElementos[0]) && elemento2.equals(lineaElementos[1])){
-
+                        matrizCombinaciones[i][j] = 2;
+                    }else {
+                        matrizCombinaciones[i][j] = -1;
                     }
                 }
+                sc.close();
             }
         }
         
-        return new int[][]{};
+        return matrizCombinaciones;
     }
 }
