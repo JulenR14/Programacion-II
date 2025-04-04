cda35b58600e4f65f216d663eb3bce1c8094a090
Julen <julen20191414@gmail.com>
2025-04-04 12:29:30 +0200

    He avanzado algunos apartados del ejercicio 5

--- EIMT1008/src/practica2/Ejercicio5/Fecha.java
+++ EIMT1008/src/practica2/Ejercicio5/Fecha.java
@@ -29,7 +29,58 @@ public class Fecha {
         return resultado;
     }
 
-    public int compareTo(Fecha fecha) {
-        return 0;
+    public int compareTo(Fecha otraFecha) {
+        int valor = 0;
+
+        if (this.year > otraFecha.year) {
+            valor = 1;
+        }else if(this.year < otraFecha.year){
+            valor = -1;
+        }else if(this.mes > otraFecha.mes){
+            valor = 1;
+        }else if (this.mes < otraFecha.mes){
+            valor = -1;
+        } else if (this.dia > otraFecha.dia) {
+            valor = 1;
+        } else if (this.dia < otraFecha.dia) {
+            valor = -1;
+        }
+        return valor;
+    }
+
+    public int getDía() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAño() {
+        return year;
+    }
+
+    public static boolean esAñoBisiesto(int año){
+        boolean bisiesto = false;
+        if((año%4 == 0 && año%100 != 0) || (año%400 == 0)){
+            bisiesto = true;
+        }
+        return bisiesto;
+    }
+
+    public static int díasMes(int mes, int año){
+        int dias = 0;
+        switch (mes){
+            case 1,3,5,7,8,10,12 -> dias = 31;
+            case 2 -> {
+                if(esAñoBisiesto(año)){
+                    dias = 29;
+                }else{
+                    dias = 28;
+                }
+            }
+            case 4,6,9,11 -> dias = 30;
+        }
+        return dias;
     }
 }
