4d3e68b99e4727aaafdc29cfe8fb971359aea648
Julen <julen20191414@gmail.com>
2025-05-16 09:05:20 +0200

    Pruebas de los métodos add del ejercicio 2 de la practica 3

--- EIMT1008/src/Practica3/Ejercicio2/MainEjercicio2.java
+++ EIMT1008/src/Practica3/Ejercicio2/MainEjercicio2.java
@@ -1,9 +1,41 @@
 package Practica3.Ejercicio2;
 
+import java.awt.*;
+
 public class MainEjercicio2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ListaCadenasEnlaceSimple lista = new ListaCadenasEnlaceSimple();
 
-        System.out.println("");
+        System.out.println("Lista vacía :");
+        System.out.println(lista);
+
+        lista.add("Julen");
+        lista.add("Marcos");
+        lista.add("Ana");
+        lista.add("Luis");
+        lista.add("Alex");
+
+        System.out.println("Se ha agregad nombres a la lista");
+        System.out.println(lista);
+
+        lista.add(2, "Marta");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Se agrega Marta en la posicion 2.");
+        System.out.println(lista);
+
+        lista.add(0, "Pedro");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Se agrega Pedro en la posicion 0.");
+        System.out.println(lista);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Se agrega Pedro en la posicion 15.");
+        try {
+            lista.add(15, "Maria");
+            System.out.println(lista);
+        } catch (Exception e) {
+            System.out.println("Esta posición no existe");
+        }
+
     }
 }
